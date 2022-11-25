6a135403f83921441123380745ae299131b4a543
rose534 <wambuirose534@gmail.com>
2022-11-25 13:56:45 +0300

    first-class-functions

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    const name = prompt("Please enter your name.");
+    callback(name);
+  }
+
+function returnsANamedFunction() {
+    
+    return returnsANamedFunction;
+    
+  }
+  
+const returnsAnAnonymousFunction = function() {
+    return (function(){
+        
+    })
+}
